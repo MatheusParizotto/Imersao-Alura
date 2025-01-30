80212710dee570b42837774731cee1564ee38d3a
Matheus Parizotto <matheusparizotto29@gmail.com>
2025-01-30 11:43:42 -0400

    Desenvolvendo com js (Salvando aqui caso aconteça alguma merda)

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('foi')
+const searchInput = document.getElementById('search-input');
+const resultsArtists = document.getElementById('result-artist');
+
+document.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === ''){
+
+    }
+})
