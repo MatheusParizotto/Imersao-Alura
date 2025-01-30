13f7005d9962cfbc6415b3320d10e72c7403da10
Matheus Parizotto <matheusparizotto29@gmail.com>
2025-01-30 12:03:45 -0400

    Copiando API e avançando no js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const searchInput = document.getElementById('search-input');
 const resultsArtists = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
 
 document.addEventListener('input', function() {
     const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === ''){
-
+        resultPlaylist.classList.add('hidden');
+        resultsArtists.classList.remove('hidden');
+        return;
     }
+
+    requestApi(searchTerm);
 })
